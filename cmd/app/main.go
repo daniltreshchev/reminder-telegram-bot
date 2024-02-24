3b41ec4ba20e74424b63cfeb7ad5273b754831db
Danil Treshchev <Madaspeee@yandex.ru>
2024-02-24 09:18:03 +0500

    add: chains

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"practice-telegram-bot/internal/app"
+	"practice-telegram-bot/internal/app/chains"
 	"practice-telegram-bot/internal/app/handlers"
 	"practice-telegram-bot/internal/config"
 	"practice-telegram-bot/pkg/api"
@@ -18,15 +19,26 @@ func main() {
 	}
 
 	botApi := api.API{Token: token}
-	botDispatcher := dispatcher.Dispatcher{Commands: make(map[string]dispatcher.Command)}
+	botDispatcher := dispatcher.NewDispatcher()
 
 	fmt.Println(botApi.GetMe())
 
-	helpCommand := dispatcher.Command{Name: "help", Handler: func(event types.Update, api api.API) { handlers.Hello(event, api) }}
+	helpCommand := dispatcher.Command{Name: "help", Handler: func(event types.Update) { handlers.Hello(event, botApi, &botDispatcher) }}
+	addCommand := dispatcher.Command{Name: "add", Handler: func(event types.Update) { handlers.Add(event, botApi, &botDispatcher) }}
 
-	botDispatcher.AddCommand(helpCommand)
+	addChain := dispatcher.Chain{
+		StartChainCommand: addCommand,
+		Handlers: []func(event types.Update){
+			func(event types.Update) { chains.AddChainFirstPhrase(event, botApi, &botDispatcher) },
+			func(event types.Update) { chains.AddChainSecondPhrase(event, botApi, &botDispatcher) },
+			func(event types.Update) { chains.AddChainThirdPhrase(event, botApi, &botDispatcher) },
+		},
+	}
+
+	botDispatcher.AddChains([]dispatcher.Chain{addChain})
+	botDispatcher.AddCommands([]dispatcher.Command{helpCommand, addCommand})
 
 	params := types.UpdateRequestParams{Limit: 100}
 
-	app.Run(botApi, botDispatcher, params)
+	app.Run(botApi, &botDispatcher, params)
 }
