124c068c4ab93345a1ecc9adc51a1b690b9fc6ed
Danil Treshchev <Madaspeee@yandex.ru>
2024-02-24 07:39:31 +0500

    refactor: move bot init login in main package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "practice-telegram-bot/internal/app"
+import (
+	"fmt"
+	"practice-telegram-bot/internal/app"
+	"practice-telegram-bot/internal/app/handlers"
+	"practice-telegram-bot/internal/config"
+	"practice-telegram-bot/pkg/api"
+	"practice-telegram-bot/pkg/dispatcher"
+	"practice-telegram-bot/pkg/types"
+)
 
 func main() {
-	app.Run()
+	token, err := config.GetApiToken()
+
+	if err != nil {
+		panic(err)
+	}
+
+	botApi := api.API{Token: token}
+	botDispatcher := dispatcher.Dispatcher{Commands: make(map[string]dispatcher.Command)}
+
+	fmt.Println(botApi.GetMe())
+
+	helpCommand := dispatcher.Command{Name: "help", Handler: func(event types.Update, api api.API) { handlers.Hello(event, api) }}
+
+	botDispatcher.AddCommand(helpCommand)
+
+	params := types.UpdateRequestParams{Limit: 100}
+
+	app.Run(botApi, botDispatcher, params)
 }
--- internal/app/bot.go
+++ internal/app/bot.go
@@ -2,30 +2,12 @@ package app
 
 import (
 	"fmt"
-	"practice-telegram-bot/internal/app/handlers"
-	"practice-telegram-bot/internal/config"
 	"practice-telegram-bot/pkg/api"
 	"practice-telegram-bot/pkg/dispatcher"
 	"practice-telegram-bot/pkg/types"
 )
 
-func Run() {
-	token, err := config.GetApiToken()
-
-	if err != nil {
-		panic(err)
-	}
-
-	botApi := api.API{Token: token}
-	botDispatcher := dispatcher.Dispatcher{Commands: make(map[string]dispatcher.Command)}
-
-	fmt.Println(botApi.GetMe())
-
-	helpCommand := dispatcher.Command{Name: "help", Handler: func(event types.Update, api api.API) { handlers.Hello(event, api) }}
-
-	botDispatcher.AddCommand(helpCommand)
-
-	params := types.UpdateRequestParams{Limit: 100}
+func Run(botApi api.API, botDispatcher dispatcher.Dispatcher, params types.UpdateRequestParams) {
 	for {
 		updates, err := botApi.GetUpdates(params)
 
